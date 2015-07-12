50b91a8889837ac3f2e3d2871680b844b6023b7c
Jonathan Frederic <jon.freder@gmail.com>
2015-07-11 23:06:22 -0500

    Remove btoa

--- src/imagerenderer.js
+++ src/imagerenderer.js
@@ -1,17 +1,17 @@
 "use strict";
 
 import {RendererBase} from './rendererbase';
-import {btoa} from 'btoa';
 
+/**
+ * Converts b64 image mimetypes to img elements.
+ */
 export class ImageRenderer extends RendererBase {
-    constructor(mimetype, b64) {
+    constructor(mimetype) {
         this.mimetype = mimetype;
-        this.b64 = b64 || false;
     }
 
     transform(data, doc) {
         let img = (doc || document).createElement('img');
-        let b64data = this.b64 ? data : btoa(data);
         img.src = 'data:' + this.mimetype + ';base64,'+ data;
         return img;
     }
