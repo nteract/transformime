3d774d63669f2c893f29f4c82c1c99c71591377f
Kyle Kelley <rgbkrk@gmail.com>
2015-07-17 18:32:11 -0500

    A failing test on purpose!

--- test/test.js
+++ test/test.js
@@ -53,12 +53,12 @@ describe('Transformime defaults', function() {
 });
 
 describe('Transformime', function() {
-    before(function() {
-        this.dummyRenderer = new DummyRenderer("transformime/dummy1");
+    beforeEach(function() {
+        this.dummyRenderer1 = new DummyRenderer("transformime/dummy1");
         this.dummyRenderer2 = new DummyRenderer("transformime/dummy2");
         this.dummyRenderer3 = new DummyRenderer("transformime/dummy3");
         this.renderers = [
-            this.dummyRenderer,
+            this.dummyRenderer1,
             this.dummyRenderer2,
             this.dummyRenderer3
         ];
@@ -69,8 +69,8 @@ describe('Transformime', function() {
         it('should have called our DummyRender', function() {
             var el = this.t.transform("dummy-data", "transformime/dummy1", this.document);
 
-            assert.equal(this.dummyRenderer.lastData, "dummy-data");
-            assert.equal(this.dummyRenderer.lastDoc, this.document);
+            assert.equal(this.dummyRenderer1.lastData, "dummy-data");
+            assert.equal(this.dummyRenderer1.lastDoc, this.document);
 
             // el should be an HTMLElement, which only exists in jsdom or on a
             // real document.
@@ -84,22 +84,60 @@ describe('Transformime', function() {
     describe('getRenderer', function() {
         it('should get the right renderer for a given mimetype', function() {
             let renderer = this.t.getRenderer('transformime/dummy1');
-            assert.equal(this.dummyRenderer, renderer);
+            assert.equal(this.dummyRenderer1, renderer);
         });
         it('should return null with an unknown mimetype', function() {
             assert.isNull(this.t.getRenderer('cats/calico'), 'found a renderer when I shouldn\'t have');
         });
     });
     describe('transformRichest', function() {
-        it('should choose the lastmost of the renderers when picking from a rich set', function() {
-            let mimeBundle = {
-                'transformime/dummy1': 'dummy data 1',
-                'transformime/dummy2': 'dummy data 2',
-                'transformime/dummy3': 'dummy data 3'
-            };
+        describe('should only render the "richest" of the renderers', function() {
+            it('when called with a all mimetypes in the mimebundle, only return lastmost', function() {
+                let mimeBundle = {
+                    'transformime/dummy1': 'dummy data 1',
+                    'transformime/dummy2': 'dummy data 2',
+                    'transformime/dummy3': 'dummy data 3'
+                };
 
-            this.t.transformRichest(mimeBundle, this.document);
+                let el = this.t.transformRichest(mimeBundle, this.document);
+                assert.isUndefined(this.dummyRenderer1.lastData);
+                assert.isUndefined(this.dummyRenderer2.lastData);
+                assert(this.dummyRenderer3.lastData, "dummy data 3");
+            });
+            it('when called with a lesser mimebundle, choose most rich', function() {
+                let mimeBundle = {
+                    'transformime/dummy1': 'dummy data 1',
+                    'transformime/dummy2': 'dummy data 2'
+                };
 
+                let el = this.t.transformRichest(mimeBundle, this.document);
+                assert.isUndefined(this.dummyRenderer1.lastData);
+                assert(this.dummyRenderer2.lastData, "dummy data 2");
+                assert.isUndefined(this.dummyRenderer3.lastData);
+            });
+            it('when called with mimetypes it doesn\'t know, it uses supported mimetypes', function() {
+                let mimeBundle = {
+                    'video/quicktime': 'cat vid',
+                    'transformime/dummy1': 'dummy data 1',
+                    'application/x-shockwave-flash': 'flashy',
+                    'application/msword': 'DOC',
+                    'application/zip': 'zippy'
+                };
+
+                let el = this.t.transformRichest(mimeBundle, this.document);
+                assert(this.dummyRenderer1.lastData, "dummy data 1");
+                assert.isUndefined(this.dummyRenderer2.lastData);
+                assert.isUndefined(this.dummyRenderer3.lastData);
+            });
+            it('when called with no supported mimetypes, it throws an error', function(){
+                let mimeBundle = {
+                    'video/quicktime': 'cat vid',
+                    'application/zip': 'zippy'
+                };
+
+                expect(() => this.t.transformRichest(mimeBundle, this.document))
+                    .to.throw('Renderer(s) for mimetype video/quicktime, application/zip not found.');
+            });
         });
     });
 });
