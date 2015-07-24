00df09b54106d3a9eea16fa39fe5fb0eb7823d7d
Kyle Kelley <rgbkrk@gmail.com>
2015-07-24 13:11:58 -0700

    Text test

---
+++ test/textTransformerTest.js
@@ -0,0 +1,27 @@
+import {assert} from 'chai';
+
+import {jsdom} from 'jsdom';
+
+import {TextTransformer} from '../src/texttransformer';
+
+describe('text transformer', function() {
+    beforeEach(function() {
+        this.document = jsdom();
+    });
+
+    it('should have the text/plain mimetype', function() {
+        var t = new TextTransformer();
+        assert.equal(t.mimetype, "text/plain");
+    });
+    describe('#transform', function() {
+        it('should create a pre with all the passed in elements', function() {
+            var t = new TextTransformer();
+
+            var text = "There is no text but text.\nWoo.";
+            var el = t.transform(text, this.document);
+            assert.equal(el.innerHTML, text);
+            assert.equal(el.textContent, text);
+            assert.equal(el.localName, "pre");
+        });
+    });
+});
