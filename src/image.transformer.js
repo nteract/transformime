65442de1d921b20ea3b9099057015ec4acc5a13a
Kyle Kelley <rgbkrk@gmail.com>
2015-08-10 23:57:36 -0500

    Try out JS Standard Style

--- src/image.transformer.js
+++ src/image.transformer.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict'
 
 /**
  * Converts b64 image mimetypes to img elements.
  */
-export function ImageTransformer(mimetype, data, document) {
-    let img = document.createElement('img');
-    img.src = 'data:' + mimetype + ';base64,'+ data;
-    return img;
+export function ImageTransformer (mimetype, data, document) {
+  let img = document.createElement('img')
+  img.src = 'data:' + mimetype + ';base64,' + data
+  return img
 }
-ImageTransformer.mimetype = ['image/png', 'image/jpeg', 'image/gif'];
+ImageTransformer.mimetype = ['image/png', 'image/jpeg', 'image/gif']
