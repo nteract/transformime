bbcc306acfaea03524781e395f56499e3ef6d588
Kyle Kelley <rgbkrk@gmail.com>
2015-11-05 23:38:39 -0600

    Rely on the Range API.

--- src/html.transformer.js
+++ src/html.transformer.js
@@ -1,10 +1,7 @@
 'use strict'
 
 export function HTMLTransformer (mimetype, data, document) {
-  var em = document.createElement('embed')
-  var URL = document.defaultView.URL
-  var Blob = document.defaultView.Blob
-  em.src = URL.createObjectURL(new Blob([data], {type: mimetype}))
-  return em
+  var range = document.createRange()
+  return range.createContextualFragment(data)
 }
 HTMLTransformer.mimetype = 'text/html'
