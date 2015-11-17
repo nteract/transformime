d7c9428de862d1db091f28cf19e8c41aaedacf58
Safia Abdalla <safia@safia.rocks>
2015-11-16 21:03:09 -0600

    Removed old transformer files

--- src/html.transformer.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-export function HTMLTransformer (mimetype, data, document) {
-  var range = document.createRange()
-  return range.createContextualFragment(data)
-}
-HTMLTransformer.mimetype = 'text/html'
--- src/text.transformer.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-export function TextTransformer (mimetype, data, document) {
-  var el = document.createElement('pre')
-  el.textContent = data
-  return el
-}
-TextTransformer.mimetype = 'text/plain'
